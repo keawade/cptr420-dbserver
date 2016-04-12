b1945c8d8db6614f171ac2d471f3e600dae196ab
Keith Wade <keithwade@nanonation.net>
2016-04-12 12:08:09 -0500

    Create and read implemented

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 
 var app = express()
 
@@ -15,15 +16,39 @@ app.set('view engine', 'pug')
 app.set('port', process.env.PORT || 3000)
 app.set('ip', process.env.IP || 'localhost')
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
 app.use(express.static('public'))
 app.locals.sitename = 'CPTR420'
 app.locals.source_url = 'https://github.com/UnionTTC/cptr420-dbserver'
 
 var Data = require('./models/data.js')
+
 app.get('/', function (req, res) {
-  res.render('index', {
-    title: 'index',
-    records: Data.find().select('item')
+  Data.find(function (err, info) {
+    if (err) {
+      console.log(err)
+    }
+    res.render('index', {
+      title: 'index',
+      records: info
+    })
+  })
+})
+
+app.post('/', function (req, res) {
+  var temp = new Data()
+  var tempData = {
+    item: req.body.textinput
+  }
+  temp.set(tempData)
+  temp.save(function (err) {
+    if (err) {
+      console.log(err)
+    }
+    res.redirect('/')
   })
 })
 
